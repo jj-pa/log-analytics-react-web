e412d1e44971117ef11113d995af5f0bed45a38e
JJ-PA <jj-pa@JETBOOK.local>
2020-02-16 20:33:51 +0900

    responsive d3 component

--- src/views/TableDashboard/TableDashboard.js
+++ src/views/TableDashboard/TableDashboard.js
@@ -7,8 +7,6 @@ import { range } from 'd3-array';
 import {
   LatestSales,
   UsersByDevice,
-  BarChart,
-  Bar,
   AnimatedBar
 } from './components';
 
@@ -20,20 +18,6 @@ const useStyles = makeStyles(theme => ({
 
 const TableDashboard = () => {
   const classes = useStyles();
-
-  const generateData = (value, length = 10) =>
-    range(length).map((item, index) => ({
-      index: index,
-      date: index,
-      value: value === null || value === undefined ?
-        Math.random() * 100 : value
-    }))
-
-  const [data, setData] = useState(generateData());
-  const changeData = () => {
-    // setData(generateData(null, Math.floor(Math.random() * 10 + 1)));
-    setData(generateData())
-  }
 
   return (
     <div className={classes.root}>
@@ -66,37 +50,7 @@ const TableDashboard = () => {
           xl={12}
           xs={12}
         >
-          <BarChart data={[5,10,1,3]} size={[500, 500]} />
-        </Grid>
-        <Grid
-          item
-          lg={12}
-          md={12}
-          xl={12}
-          xs={12}
-        >
-          <span className='label'>Bar SVG</span>
-          <Bar
-            data={data}
-            width={600}
-            height={400}
-            top={20}
-            bottom={30}
-            left={0}
-            right={0}
-          />
-        </Grid>
-        <Grid
-          item
-          lg={12}
-          md={12}
-          xl={12}
-          xs={12}
-        >
-          <button onClick={changeData}>Transform</button>
-          <span className='label'>Animated Bar SVG (React Spring)</span>
           <AnimatedBar
-            data={data}
             width={600}
             height={400}
             top={20}
--- src/views/TableDashboard/components/AnimatedBar/AnimatedBar.js
+++ src/views/TableDashboard/components/AnimatedBar/AnimatedBar.js
@@ -1,178 +1,99 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Divider,
+  Button
+} from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
-import { animated, useSpring } from 'react-spring';
-import { select } from 'd3-selection'
-import { scaleBand, scaleLinear, scaleOrdinal } from 'd3-scale';
-import { schemeCategory10 } from 'd3-scale-chromatic'
-import { format } from 'd3-format'
-import { axisBottom, axisLeft } from 'd3-axis'
-import { range, max as maxD } from 'd3-array';
-import { interpolate } from 'd3-interpolate';
-import { transition } from 'd3-transition';
+import AnimatedBarChart from './AnimatedBarChart';
+import { range } from 'd3-array';
 
-const colors = scaleOrdinal(schemeCategory10);
-const formatD = format('.2f');
-const animationDuration = 150;
-const animationConfig = {
-  to: async (next, cancel) => {
-    await next({ t: 1 });
+import './chart-styles.css'
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  chartContainer: {
+    height: 400,
+    position: 'relative'
   },
-  from: { t: 0 },
-  config: { duration: animationDuration },
-  reset: true
-};
-
-const XAxis = ({ top, bottom, left, right, height, scale }) => {
-  const axis = useRef(null);
-
-  useEffect(() => {
-    select(axis.current)
-      .transition()
-      .duration(animationDuration)
-      .call(axisBottom(scale));
-  });
-
-  return (
-    <g
-      className="axis x"
-      ref={axis}
-      transform={`translate(${left}, ${height - bottom})`}
-    />
-  );
-};
-
-const YAxis = ({ top, bottom, left, right, scale }) => {
-  const axis = useRef(null);
-
-  useEffect(() => {
-    select(axis.current)
-      .transition()
-      .duration(animationDuration)
-      .call(axisLeft(scale));
-  });
-
-  return (
-    <g className="axis y" ref={axis} transform={`translate(${left}, ${top})`} />
-  );
-};
-
-const Rect = ({ index, data, prev, next, x, y, height, top, bottom, sort }) => {
-  const [animatedProps, setAnimatedProps] = useSpring(() => animationConfig);
-  setAnimatedProps(animationConfig);
-
-  const prevIndex = prev.findIndex(d => d.index === next[index].index);
-  const interpolator = interpolate(prev[index], data);
-  const shouldUpdate =
-    !sort &&
-    prev[index].index === data.index &&
-    prev[index].value !== data.value;
-
-  const interpolatorX = interpolate(
-    x(sort ? prevIndex : prevIndex),
-    x(sort ? data.index : data.index)
-  );
-
-  const interpolatorY = interpolate(
-    y(shouldUpdate ? prev[index].value : data.value),
-    y(data.value)
-  );
-
-  return (
-    <animated.g
-      key={data.index}
-      transform={animatedProps.t.interpolate(t => {
-        return `translate(${interpolatorX(t)}, ${interpolatorY(t)})`;
-      })}
-    >
-      <animated.rect
-        width={x.bandwidth()}
-        height={animatedProps.t.interpolate(t => {
-          return height - bottom - top - interpolatorY(t);
-        })}
-        fill={colors(data.index)}
-      />
-      <animated.text
-        transform={`translate(${x.bandwidth() / 2}, ${-4})`}
-        textAnchor="middle"
-        alignmentBaseline="middle"
-        fill="grey"
-        fontSize="10"
-      >
-        {shouldUpdate
-          ? animatedProps.t.interpolate(t => formatD(interpolator(t).value))
-          : formatD(data.value)}
-      </animated.text>
-    </animated.g>
-  );
-};
+  actions: {
+    justifyContent: 'flex-end'
+  }
+}));
 
 const AnimatedBar = props => {
-  const cache = useRef(props.data);
-  const data = useRef(props.data);
-  const [sort, setSort] = useState(false);
+  const { className, ...rest } = props;
 
-  data.current = sort
-    ? [...props.data].sort((a, b) => b.value - a.value)
-    : [...props.data];
+  const classes = useStyles();
 
-  const x = scaleBand()
-    .range([0, props.width - props.left - props.right])
-    .domain(data.current.map(d => d.date))
-    .padding(0.1);
+  const generateData = (value, length = 10) =>
+    range(length).map((item, index) => ({
+      index: index,
+      date: index,
+      value: value === null || value === undefined ?
+        Math.random() * 100 : value
+    }))
 
-  const y = scaleLinear()
-    .range([props.height - props.top - props.bottom, 0])
-    .domain([0, maxD(data.current, d => d.value)]);
+  // const [data, setData] = useState(generateData());
+  const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
 
-  useEffect(() => {
-    cache.current = data.current;
-  });
+  const changeData = () => {
+    // setData(generateData(null, Math.floor(Math.random() * 10 + 1)));
+    setData(generateData())
+  }
 
   return (
-    <>
-      <button
-        onClick={() => {
-          setSort(!sort);
-        }}
-      >
-        Toggle sort
-      </button>
-      <svg width={props.width} height={props.height}>
-        <XAxis
-          scale={x}
-          top={props.top}
-          bottom={props.bottom}
-          left={props.left}
-          right={props.right}
-          height={props.height}
-        />
-        <YAxis
-          scale={y}
-          top={props.top}
-          bottom={props.bottom}
-          left={props.left}
-          right={props.right}
-        />
-        <g transform={`translate(${props.left}, ${props.top})`}>
-          {props.data.map((d, i) => (
-            <Rect
-              key={i}
-              index={i}
-              data={d}
-              prev={cache.current}
-              next={data.current}
-              x={x}
-              y={y}
-              top={props.top}
-              bottom={props.bottom}
-              height={props.height}
-              sort={sort}
-            />
-          ))}
-        </g>
-      </svg>
-    </>
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardHeader
+        action={
+          <Button
+            size="small"
+            variant="text"
+          >
+            Last 7 days <ArrowDropDownIcon />
+          </Button>
+        }
+        title="AnimatedBar"
+      />
+      <Divider />
+      <CardContent>
+        <div className={classes.chartContainer}>
+          <span className='label'>Animated Bar SVG (React Spring)</span>
+          <div>
+            <button onClick={changeData}>Transform</button>
+          </div>
+          <AnimatedBarChart
+            data={data}
+          />
+        </div>
+      </CardContent>
+      <Divider />
+      <CardActions className={classes.actions}>
+        <Button
+          color="primary"
+          size="small"
+          variant="text"
+        >
+          Overview <ArrowRightIcon />
+        </Button>
+      </CardActions>
+    </Card>
   );
+};
+
+AnimatedBar.propTypes = {
+  className: PropTypes.string
 };
 
 export default AnimatedBar;
